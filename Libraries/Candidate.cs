451479493f81037b5fda2fc0c73e9727b50b5da2
Riane <riane.dossantos75@gmail.com>
2022-04-27 20:19:59 -0300

    Added class libraries

--- Libraries/Candidate.cs
+++ Libraries/Candidate.cs
@@ -2,5 +2,13 @@
 {
     public class Candidate
     {
+        public string Name { get; set; }
+        public long CPF { get; set; }
+        public string Email { get; set; }
+        public long Phone { get; set; }
+        public string Gender { get; set; }
+        public DateTime Bithdate { get; set; }
+        public Skill[] Skill { get; set; }
+        public Certification[] Certification { get; set; }
     }
 }
--- Libraries/Certification.cs
+++ Libraries/Certification.cs
@@ -2,5 +2,9 @@
 {
     public class Certification
     {
+        public string Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime ExpeditionDate { get; set; }
+        public DateTime ExpirationDate { get; set; }
     }
 }
